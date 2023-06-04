1da6d2913f5c86843925d3855a9d176b0eb710ff
Nehan Tarefder <58123706+nehangit@users.noreply.github.com>
2023-06-04 11:47:53 -0600

    SHA1/SHA2 Hash Cracker
    need to add support for MD5 and SHA3 in the future

---
+++ sha_hash_crack.py
@@ -0,0 +1,82 @@
+# Crack SHA (Secure Hashing Algorithm) Hashes
+# Author: Nehan Tarefder
+
+import itertools
+from sys import exit
+import argparse
+import hashlib
+
+parser = argparse.ArgumentParser(description='Crack a SHA hash (will add support for MD5 and SHA3 hashes in the future)...')
+parser.add_argument('hash', type=str, help='Specify the SHA1/SHA2 hash to crack. SUPPORTED TYPES: SHA256, SHA384, SHA224, SHA512, SHA1')
+parser.add_argument('-w', '--wordlist', type=str, help='Specify a wordlist (or path to wordlist)')
+args = parser.parse_args()
+
+thehash = args.hash
+# checking hash type
+if(len(thehash) == 64):
+  typ = 256
+elif(len(thehash) == 96):
+  typ = 384
+elif(len(thehash) == 56):
+  typ = 224
+elif(len(thehash) == 128):
+  typ = 512
+elif(len(thehash) == 40):
+  typ = 1
+else:
+  print("Input hash is not of supported type... Exiting.")
+  exit(0)
+
+def dictionary(inphash, wordlist):
+  with open(wordlist,'r') as file:  
+    for line in file:
+      for word in line.split():
+        if(typ==256):
+          tester = hashlib.sha256(word.encode())
+        elif(typ==384):
+          tester = hashlib.sha384(word.encode())
+        elif(typ==224):
+          tester = hashlib.sha224(word.encode())
+        elif(typ==512):
+          tester = hashlib.sha512(word.encode())
+        elif(typ==1):
+          tester = hashlib.sha1(word.encode())        
+        res = tester.hexdigest()
+        if (res == inphash):
+          print('*' * 20)
+          print('Password found: %s' % word)
+          exit(0)
+  print('Dictionary attack failed..')
+  bruteforce(inphash)
+
+def bruteforce(inphash):
+  # Can add other symbols to alphabet if needed
+  alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  print('Bruteforcing passwords...')
+  # For every possible combination from alphabet...
+  for i in range(1, 11):    # change these values to change the min/max password lengths to bruteforce
+    print('Testing passwords of length '+str(i)+'..')
+    for c in itertools.product(alphabet, repeat=i):
+      word = ''.join(c)
+      if(typ==256):
+          tester = hashlib.sha256(word.encode())
+      elif(typ==384):
+          tester = hashlib.sha384(word.encode())
+      elif(typ==224):
+          tester = hashlib.sha224(word.encode())
+      elif(typ==512):
+          tester = hashlib.sha512(word.encode())
+      elif(typ==1):
+          tester = hashlib.sha1(word.encode())        
+      res = tester.hexdigest()
+      if (res == inphash):
+          print('*' * 20)
+          print('Password found: %s' % word)
+          exit(0)
+
+if(args.wordlist==None):
+  bruteforce(thehash)
+else:
+  dictionary(thehash, args.wordlist)
+# If no password was found by the end..
+print('Password not found.')
